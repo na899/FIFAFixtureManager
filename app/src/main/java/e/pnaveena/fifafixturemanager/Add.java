6680c1728f7fcb3004b0ede1f283cc0fe34a1658
na899 <pnaveena1999@gmail.com>
2018-06-07 16:49:12 +0530

    list formed

--- app/src/main/java/e/pnaveena/fifafixturemanager/Add.java
+++ app/src/main/java/e/pnaveena/fifafixturemanager/Add.java
@@ -1,32 +1,79 @@
 package e.pnaveena.fifafixturemanager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 public class Add extends AppCompatActivity {
+    EditText nameA;
+    EditText nameB;
+    EditText Date;
+    EditText Time;
+    EditText venue;
 
 
-public void take(View v){
-    Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    startActivityForResult(takePicture, 0);//zero can be replaced with any action code
-}
-public void choose(View v){
-    Intent pickPhoto = new Intent(Intent.ACTION_PICK,
-            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-    startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
-}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        nameA=findViewById(R.id.teamA);
+         nameB=findViewById(R.id.teamB);
+        EditText Date=findViewById(R.id.date);
+        EditText Time=findViewById(R.id.time);
+        EditText venue=findViewById(R.id.venue);
+
+
+
+
+
     }
+    public void addFixture(View v){
+
+
+
+        SharedPreferences a=getSharedPreferences("Data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=a.edit();
+        editor.putString("teamA",nameA.getText().toString());
+        editor.putString("teamB",nameB.getText().toString());
+
+        editor.apply();
+
+
+
+      String str=a.getString("teamA","---")+" Vs. "+a.getString("teamB","---");
+MainActivity.listItems.add(str);
+        MainActivity.adapter.notifyDataSetChanged();
+/*
+
+        // Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show();*/
+      Intent intent=new Intent(this,MainActivity.class);
+
+     startActivity(intent);
+
+    }
+
+    public void take(View v){
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePicture, 0);//zero can be replaced with any action code
+    }
+    public void choose(View v){
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
+    }
+
 
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
@@ -34,7 +81,7 @@ public void choose(View v){
         switch(requestCode) {
             case 0:
 
-               
+
                 if(resultCode == RESULT_OK){
                     Uri selectedImage = imageReturnedIntent.getData();
                     imageview.setImageURI(selectedImage);
@@ -50,3 +97,4 @@ public void choose(View v){
         }
     }
 }
+
--- app/src/main/java/e/pnaveena/fifafixturemanager/MainActivity.java
+++ app/src/main/java/e/pnaveena/fifafixturemanager/MainActivity.java
@@ -1,46 +1,71 @@
 package e.pnaveena.fifafixturemanager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
-public class MainActivity extends AppCompatActivity {
-    private Button Add;
-    private Button update;
-    ArrayList<String> listItems=new ArrayList<String>();
-    ArrayAdapter<String> adapter;
-    private ListView list;
-    private EditText text;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+     public static ListView list;
+     public static ArrayList<String>listItems=new ArrayList<String >();
+     public static ArrayAdapter<String> adapter;
+     public static int i ;
+
+   // public static int[] img1={R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background};
+    //public static int[] img2={R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_foreground};
+    public  static Context c;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        c=this;
+        SharedPreferences a=getSharedPreferences("Data",MODE_PRIVATE);
+        // listItems=a.getString("listitems","---").split(",");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text=findViewById(R.id.text);
-        list =findViewById(R.id.list);
-        update=findViewById(R.id.update);
-
-
-        adapter=new ArrayAdapter<String >(this,android.R.layout.simple_spinner_item, listItems);
+        list=findViewById(R.id.list);
+         adapter= new ArrayAdapter<String>(this,R.layout.row,R.id.txt,listItems);
         list.setAdapter(adapter);
 
-    }
- public void test(View v){
-     listItems.add(text.getText().toString());
-     // next thing you have to do is check if your adapter has changed
-     adapter.notifyDataSetChanged();
- }
 
-    public void addFixture(View v){
-        Intent intent=new Intent(this,Add.class);
+    }
+
+    public void add(View v){
+
+    Intent intent=new Intent(this,Add.class);
         startActivity(intent);
 
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 }
+
+
+
+
+
+
+
+
